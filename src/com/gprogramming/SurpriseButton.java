9020562516621595414ebf03538d6255e9fcd0ba
Kushagra Sethi <ksethapp@students.cs.ubc.ca>
2023-01-31 21:28:11 -0800

    added SurpriseButton

---
+++ src/com/gprogramming/SurpriseButton.java
@@ -0,0 +1,17 @@
+package com.gprogramming;
+
+import javax.swing.*;
+import javax.swing.border.EtchedBorder;
+import java.awt.*;
+
+public class SurpriseButton extends JButton {
+    public SurpriseButton() {
+        setText("Click for a surprise!");
+        setFont(new Font("Times New Roman",Font.BOLD,24));
+        setBackground(Color.PINK);
+        setFocusPainted(false);
+        setOpaque(true);
+        setPreferredSize(new Dimension(UI.HEART_PANEL_WIDTH, UI.HEART_PANEL_HEIGHT / 10));
+        setBorder(new EtchedBorder());
+    }
+}
