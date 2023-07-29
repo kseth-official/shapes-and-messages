c3c278a2a7967da2ffa202d7df403f2556a0068f
Kush Sethi <kushagras.collegeapps@gmail.com>
2023-07-28 21:59:19 -0700

    Removed switch on type code smell from HeartPanel

--- src/com/sam/panels/polygons/HeartPanel.java
+++ src/com/sam/panels/polygons/HeartPanel.java
@@ -4,12 +4,15 @@ import com.sam.enums.HeartAnimation;
 import com.sam.UI;
 import com.sam.observation.Observer;
 import com.sam.observation.Subject;
+import com.sam.utility.animation.heart.HeartAnimator;
+import com.sam.utility.animation.heart.HeartAnimatorGrowing;
+import com.sam.utility.animation.heart.HeartAnimatorRotating;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.Path2D;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 
 public class HeartPanel extends JPanel implements Subject {
@@ -19,13 +22,7 @@ public class HeartPanel extends JPanel implements Subject {
     private int y;
     private int w;
     private int h;
-    private int red;
-    private int green;
-    private int blue;
-    private int alpha = 255;
     private int colourChangeCounter = 0;
-
-    private int polygonScale;
     private Color heartColor = Color.BLACK;
     private HeartAnimation currentAnimation;
     private List<Observer> observers = new ArrayList<>();
@@ -54,60 +51,44 @@ public class HeartPanel extends JPanel implements Subject {
 
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        // Heart scale factor and rotation calculation
-        double scale = 0;
-        double angle = 0;
+        changeHeartColor();
 
-        if (UI.isSurpriseDisplaying && currentAnimation == HeartAnimation.GROWING) {
-            scale = Math.sin(Math.PI / 2 * UI.currentFrame / UI.FRAME_RATE);
-
-            if (UI.currentFrame == UI.FRAME_RATE) {
-                currentAnimation = HeartAnimation.ROTATING;
-                scale = 1;
-                notifyObservers();
-            }
-        } else if (UI.isSurpriseDisplaying && currentAnimation == HeartAnimation.ROTATING) {
-            angle = 2 * Math.PI * UI.currentFrame / UI.FRAME_RATE;
-        } else {
-            scale = 0;
-        }
-
-        // Draw a heart of size 150x120 pixels
-        Path2D.Double heart = new Path2D.Double();
-        heart.moveTo(75, 40);
-        heart.curveTo(75, 37, 70, 25, 50, 25);
-        heart.curveTo(20, 25, 20, 62.5, 20, 62.5);
-        heart.curveTo(20, 80, 40, 102, 75, 120);
-        heart.curveTo(110, 102, 130, 80, 130, 62.5);
-        heart.curveTo(130, 62.5, 130, 25, 100, 25);
-        heart.curveTo(85, 25, 75, 37, 75, 40);
-
-        // Transform the heart to the center of the panel
-        g2d.translate(w/2,h/2);
+        HeartAnimator heartAnimator = null;
 
         if (currentAnimation == HeartAnimation.GROWING) {
-            // Scale the heart
-            g2d.scale(scale,scale);
+            if (UI.currentFrame == UI.FRAME_RATE) {
+                currentAnimation = HeartAnimation.ROTATING;
+                notifyObservers();
+            }
+            
+            heartAnimator = new HeartAnimatorGrowing();
         } else if (currentAnimation == HeartAnimation.ROTATING) {
-            // Rotate the heart by an angle
-            g2d.rotate(angle);
+            heartAnimator = new HeartAnimatorRotating();
         }
 
-        // Check if counter is a power of 2
-        if ((colourChangeCounter & (UI.FRAME_RATE / 8 - 1)) == 0) {
-            // Change the color of the heart
-            Random rng = new Random();
-            this.red = Math.abs(rng.nextInt()) & (128 - 1) + 64;
-            this.green = Math.abs(rng.nextInt()) & (128 - 1) + 64;
-            this.blue = Math.abs(rng.nextInt()) & (128 - 1) + 64;
+        if (Objects.nonNull(heartAnimator)) {
+            heartAnimator.animateGraphic(g2d, this.w, this.h, colourChangeCounter, heartColor);
         }
-
-        heartColor = new Color(red,green,blue);
-
-        g2d.setColor(heartColor);
-        g2d.fill(heart);
 
         colourChangeCounter++;
+    }
+
+    private void changeHeartColor() {
+        // Check if counter is a power of 2
+        if ((colourChangeCounter & (UI.FRAME_RATE / 8 - 1)) == 0) {
+            int red = 0;
+            int green = 0;
+            int blue = 0;
+
+            // Change the color of the heart
+            Random rng = new Random();
+
+            red = Math.abs(rng.nextInt()) & (128 - 1) + 64;
+            green = Math.abs(rng.nextInt()) & (128 - 1) + 64;
+            blue = Math.abs(rng.nextInt()) & (128 - 1) + 64;
+
+            this.heartColor = new Color(red,green,blue);
+        }
     }
 
     @Override
